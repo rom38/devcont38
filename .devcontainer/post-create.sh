f917a5a672bcb93d9a138cd97c0762a7d6527464
rom38 <rom38@users.noreply.github.com>
2026-07-17 09:54:50 +0000

    fix: remove duplicate Neovim sync command in post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -25,4 +25,5 @@ if [ ! -d /home/vscode/.config/nvim ]; then
     # nvim --headless "+Lazy! clean nvim-treesitter" +qa  || true
     # nvim --headless "+Lazy! install nvim-treesitter" +qa || true
     nvim --headless "+Lazy! sync" +qall || true
+    nvim --headless "+Lazy! sync" +qall || true
 fi
