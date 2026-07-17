6edc9a39984700105b5cbb7d3f475c1fd28bd5d1
rom38 <rom38@users.noreply.github.com>
2026-07-17 08:43:40 +0000

    fix: comment out redundant Neovim installation commands in post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -21,8 +21,8 @@ uv tool install pynvim
 # NvChad — Neovim configuration framework
 if [ ! -d /home/vscode/.config/nvim ]; then
     git clone https://github.com/rom38/nvchd_cfg /home/vscode/.config/nvim --depth 1
-    nvim --headless "+Lazy! sync" +qa || true
-    nvim --headless "+Lazy! clean nvim-treesitter" +qa  || true
-    nvim --headless "+Lazy! install nvim-treesitter" +qa || true
+    # nvim --headless "+Lazy! sync" +qa || true
+    # nvim --headless "+Lazy! clean nvim-treesitter" +qa  || true
+    # nvim --headless "+Lazy! install nvim-treesitter" +qa || true
     nvim --headless "+Lazy! sync" "+MasonInstallAll" "+TSUpdateSync" +qa || true
 fi
