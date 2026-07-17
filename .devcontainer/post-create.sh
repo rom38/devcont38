23deef64222399e13fff06ebf8d5f10e6757cf24
rom38 <rom38@users.noreply.github.com>
2026-07-17 09:48:30 +0000

    fix: remove redundant "+MasonToolsInstallSync" command from Neovim setup in post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -25,5 +25,4 @@ if [ ! -d /home/vscode/.config/nvim ]; then
     # nvim --headless "+Lazy! clean nvim-treesitter" +qa  || true
     # nvim --headless "+Lazy! install nvim-treesitter" +qa || true
     nvim --headless "+Lazy! sync" +qall || true
-    nvim --headless "+MasonToolsInstallSync" +qall || true
 fi
