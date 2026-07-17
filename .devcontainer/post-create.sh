16bfd8cfe0fb11bcb112032d52674b9ce48dd25c
rom38 <rom38@users.noreply.github.com>
2026-07-17 08:15:54 +0000

    fix: update Neovim configuration framework to clone from the correct repository

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -20,8 +20,9 @@ uv tool install pynvim
 
 # NvChad — Neovim configuration framework
 if [ ! -d /home/vscode/.config/nvim ]; then
-    git clone https://github.com/NvChad/starter /home/vscode/.config/nvim --depth 1
-    nvim --headless "+Lazy! sync" +qa
-    nvim --headless "+Lazy! clean nvim-treesitter" +qa
+    git clone https://github.com/rom38/nvchd_cfg /home/vscode/.config/nvim --depth 1
+    nvim --headless "+Lazy! sync" +qa || true
+    nvim --headless "+Lazy! clean nvim-treesitter" +qa  || true
     nvim --headless "+Lazy! install nvim-treesitter" +qa || true
+    nvim --headless "+Lazy! sync" "+MasonInstallAll" "+TSUpdateSync" +qa || true
 fi
